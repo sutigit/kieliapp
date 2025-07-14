de6db3b476b12aabcfeb0fd11343667ddee591d1
sutigit <panyanilben@gmail.com>
2025-07-14 19:10:15 +0300

    lol

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,6 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#fefefe",
-        },
-        headerTintColor: "#333",
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="courses/index" options={{ headerShown: false }} />
-      <Stack.Screen name="courses/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }
